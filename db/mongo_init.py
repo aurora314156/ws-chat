4c7cb38d32b5d1fe2b17b4cd15bb682bcae2e9ca
aurora314156 <aurora314156@gmail.com>
2025-08-27 14:11:07 +0800

    feat: add .dockerignore to separate local development and production files

--- db/mongo_init.py
+++ db/mongo_init.py
@@ -7,6 +7,7 @@ from logger import logger
 # Prefer reading MongoDB-related environment variables from the cloud environment.
 # If not available, fall back to reading from the local .env file.
 if os.getenv("ENV") == "LOCAL":
+    logger.info("Local environment detected. Loading .env file...")
     load_dotenv()  # load local .env file.
 
 
@@ -14,8 +15,10 @@ MONGO_URI = os.environ.get("MONGO_URI")
 DB_NAME = os.environ.get("DB_NAME", "chat_db")
 COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "messages")
 
-if not MONGO_URI:
-    raise ValueError("MONGO_URI must be set!")
+logger.info(f"MONGO_URI: {MONGO_URI}, DB_NAME: {DB_NAME}, COLLECTION_NAME: {COLLECTION_NAME}")
+
+if not MONGO_URI or not DB_NAME or COLLECTION_NAME:
+    logger.error("❌ MONGO_ENV parameters is not set! Please check environment variables.")
 
 # ----------------------------
 # MongoDB client
@@ -31,7 +34,6 @@ async def check_mongo_connection():
     try:
         # 使用 serverSelectionTimeoutMS 防止卡住
         await mongo_client.admin.command("ping")
-        logger.info(f"MONGO_URI: {MONGO_URI}, DB_NAME: {DB_NAME}, COLLECTION_NAME: {COLLECTION_NAME}")
         logger.info(f"[✅] MongoDB connection successful. Database '{DB_NAME}' is reachable.")
     except Exception as e:
         logger.error(f"[❌] MongoDB connection failed: {e}")
